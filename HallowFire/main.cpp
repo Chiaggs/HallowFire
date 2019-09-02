3d6941118d0fd3b6b9e52dd091fcaf821628b22f
Chiaggs <chiaggs@gmail.com>
2019-09-02 16:11:06 -0400

    Added code for Platform

--- HallowFire/main.cpp
+++ HallowFire/main.cpp
@@ -1,25 +1,68 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
 
 class Platform {
+public:
+	// Variables
+	static int objectCount;
 	sf::RectangleShape rectangle;
+	int length;
+	int breadth;
+
+	// Constrcutor
 	Platform() {
-		rectangle.setSize(sf::Vector2f(100.f, 20.f));
+		objectCount++;
+		length = 200;
+		breadth = 40;
+		rectangle.setSize(sf::Vector2f(length, breadth));
+		rectangle.setFillColor(sf::Color::White);
+		setPosition();
+	}
+
+	// Helper Functions
+	void setPosition() {
+		rectangle.setPosition(calculateHorizontalPosition(), 560.f);
+	}
+	int calculateHorizontalPosition() {
+		int buffer = 50;
+		int pos = buffer;
+		for (int i = 1; i < objectCount; i++) {
+			pos += length + buffer;
+		}
+		return pos;
+	}
+
+	// public interface functions
+	int getObjectCount() {
+		return objectCount;
 	}
 };
+int Platform::objectCount = 0;
 
 class Character {
+public:
 };
 
 class MovingPlatform {
+public:
+
 };
 
 int main()
 {
+	// Set initial configuration
 	sf::ContextSettings settings;
 	settings.antialiasingLevel = 8; // setting the anti-aliasing level, to remove jagged lines
 	sf::RenderWindow window;
 	window.create(sf::VideoMode(800, 600), "HallowFire", sf::Style::Default, settings); // Optional thrid arg can be used to specify Window style
 	window.setVerticalSyncEnabled(true); // Setting VSync to true, to prevent screen tearing
+
+	//Defing Entities
+	Platform p1, p2, p3;
+
+	//Frame Processing
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -32,9 +75,14 @@ int main()
 		window.clear(sf::Color::Black);
 
 		// Code to draw contents in the frame
+		window.draw(p1.rectangle);
+		window.draw(p2.rectangle);
+		window.draw(p3.rectangle);
 
 		// end of the current frame
 		window.display();
 	}
+
+
 	return 0;
 }
