8a8dc8b209a532d31ed5d2b46a6a06f5a2a22cd4
Chiaggs <chiaggs@gmail.com>
2019-12-06 16:36:56 -0500

    Added support for scripting

--- HallowFire_Multiplayer1/main_mplay1.cpp
+++ HallowFire_Multiplayer1/main_mplay1.cpp
@@ -9,6 +9,9 @@
 #include <thread>
 #include <future>
 #include <vector>
+#include <fstream>
+#include <streambuf>
+#include <dukglue/dukglue.h>
 #ifndef _WIN32
 #include <unistd.h>
 #else
@@ -51,6 +54,54 @@ public:
 		this->timestamp = timestamp;
 	}
 };
+
+// ----------- Code for managin scripting ---------------------
+
+class Vector2f
+{
+public:
+	Vector2f(double x, double y) : m_fX(x), m_fY(y) {}
+	Vector2f() { m_fX = 0; m_fY = 0; }
+
+	float Dot(Vector2f& other)
+	{
+		return m_fX * other.GetX() + m_fY * other.GetY();
+	}
+
+	float Distance(Vector2f& other)
+	{
+		return sqrt(pow(m_fX - other.GetX(), 2) + pow(m_fY - other.GetY(), 2));
+	}
+
+	/* Getters and setters */
+	double GetX() { return m_fX; }
+	double GetY() { return m_fY; }
+	void SetX(float x) { m_fX = x; }
+	void SetY(float y) { m_fY = y; }
+
+private:
+	double m_fX;
+	double m_fY;
+};
+
+static void load_script_from_file(duk_context* ctx, const char* filename)  
+{																		  
+	std::ifstream t(filename);
+	std::stringstream buffer;
+	buffer << t.rdbuf();
+	duk_push_lstring(ctx, buffer.str().c_str(), (duk_size_t)(buffer.str().length()));
+}
+
+static duk_ret_t native_print(duk_context* ctx)
+{
+	duk_push_string(ctx, " ");
+	duk_insert(ctx, 0);
+	duk_join(ctx, duk_get_top(ctx)- 1);
+	printf("%s\n", duk_safe_to_string(ctx, -1));
+	return 0;
+}
+
+// ------------------------------------------------------------
 
 // client multithreading arg sender
 class arg_wrapper {
@@ -261,6 +312,55 @@ int main() {
 	text.setFillColor(sf::Color::White);
 
 	// Socket Programming
+
+
+	// --------------- Scripting -----------------------
+
+	duk_context* ctx = NULL;
+
+	ctx = duk_create_heap_default();
+	if (!ctx) {
+		printf("Failed to create a Duktape heap.\n");
+		exit(1);
+	}
+	// Register objects and member functions inside our context
+	dukglue_register_constructor<Vector2f>(ctx, "Vector2f"); // Can this be done more dynamically? i.e. to allow client to register objects to work with scripts?
+	dukglue_register_method(ctx, &Vector2f::Dot, "dot");
+	dukglue_register_method(ctx, &Vector2f::Distance, "distance");
+	dukglue_register_method(ctx, &Vector2f::GetX, "x");
+	dukglue_register_method(ctx, &Vector2f::GetY, "y");
+
+	// Can use the standard duktape API to register c_functions if necessary
+	duk_push_c_function(ctx, native_print, DUK_VARARGS);
+	duk_put_global_string(ctx, "print");
+
+	// Load script from file, evaluate script
+	load_script_from_file(ctx, "testScript.js");
+	if (duk_peval(ctx) != 0) {
+		printf("Error occured: %s\n", duk_safe_to_string(ctx, -1));
+		duk_destroy_heap(ctx);
+		return 1;
+	}
+	duk_pop(ctx); // Ignore return, clear stack
+
+	duk_push_global_object(ctx);			
+	duk_get_prop_string(ctx, -1, "myTest"); 
+											
+	Vector2f a(2, 3); Vector2f b(4, 5);
+	dukglue_push(ctx, &a);
+	dukglue_push(ctx, &b);
+
+	if (duk_pcall(ctx, 2) != 0)
+		printf("Error here: %s\n", duk_safe_to_string(ctx, -1));
+	else
+		printf("%s\n", duk_safe_to_string(ctx, -1));
+
+	duk_pop(ctx);
+
+	duk_destroy_heap(ctx);
+
+	// -------------------------------------------------
+
 
 	//Frame Processing
 	while (window.isOpen())
